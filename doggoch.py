273328b3b15cb43ada56f4b507b82fb094a5dffb
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2022-04-26 14:12:41 +0200

    retrieved image

--- doggoch.py
+++ doggoch.py
@@ -7,7 +7,7 @@ import urllib.request
 response = requests.get("https://dog.ceo/api/breeds/image/random")
 jsonobj = json.loads(response.text)
 url = jsonobj["message"]
-#print(url)
+print(url)
 
 img_data = requests.get(url).content
 with open('currentdoggo.jpg', 'wb') as handler:
